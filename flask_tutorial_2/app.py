dc11989647b6ebd3899cc29da01961f17cf24d14
Bjarke Nicolaisen Lydholm <jqm470@alumni.ku.dk>
2024-05-19 12:49:58 +0200

    completed tutorial 3/5, about POST/GET methods

--- flask_tutorial_2/app.py
+++ flask_tutorial_2/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/mysite")
-def hello():
-  return "Hello, world!"
+@app.route("/", methods=["GET","POST"])
+def home():
+  print(request.form.get("amount"))
+  return render_template("form.html")
 
 if __name__ == "__main__":
     app.run(debug=True) 
